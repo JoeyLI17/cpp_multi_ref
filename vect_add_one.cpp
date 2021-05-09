77ae5ec31812ea791082d8641f2cd7fce69728b1
JLI <lijiayi17@gmail.com>
2021-05-09 17:24:39 +0800

    add file

---
+++ vect_add_one.cpp
@@ -0,0 +1,9 @@
+// call // header1.h
+#include "vect_add_one.h"
+
+void AddOneToEach(vector<int>& v)
+{
+  for (auto& i : v) {
+    i++;
+  }
+}
---
+++ vect_add_one.h
@@ -0,0 +1,11 @@
+// header1.h
+#ifndef VECT_ADD_ONE_H
+#define VECT_ADD_ONE_H
+
+#include <vector>
+using std::vector;
+
+// AddOneToEach method declaration.
+void AddOneToEach(vector<int>& v);
+
+#endif
